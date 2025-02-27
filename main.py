09ededb052fec04284087051d2b5997036f3332b
Bonus1337 <szymanski.maciej1337@gmail.com>
2025-02-27 13:56:55 +0100

    Add total product count summary to shopping list output

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ shops_and_products = {
 for shop, products in shops_and_products.items():
     products = [product.capitalize() for product in products]
     print(f"Idę do {shop.capitalize()} i kupuję tam następujące produkty: {products}")
+print("W sumie kupię", sum([len(products) for products in shops_and_products.values()]), "produktów.")
