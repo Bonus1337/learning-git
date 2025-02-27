68a75c1ae256fec2a369877aad75daf711ba376a
Bonus1337 <szymanski.maciej1337@gmail.com>
2025-02-27 13:56:15 +0100

    Capitalize product names and print shopping list for each shop

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ shops_and_products = {
     "Piekarnia" : ["Chleb", "Pączek", "Bułki"],
     "Warzywniak" : ["Marchew", "Seler", "Rukola"]
 }
+for shop, products in shops_and_products.items():
+    products = [product.capitalize() for product in products]
+    print(f"Idę do {shop.capitalize()} i kupuję tam następujące produkty: {products}")
