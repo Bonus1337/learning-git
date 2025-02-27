e73446295b793baf7e691e1f7b2e38f37a1a301d
Maciej <78831375+Bonus1337@users.noreply.github.com>
2025-02-27 14:19:08 +0100

    Add new print statement

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ for shop, products in shops_and_products.items():
     products = [product.capitalize() for product in products]
     print(f"Idę do {shop.capitalize()} i kupuję tam następujące produkty: {products}")
 print("W sumie kupię", sum([len(products) for products in shops_and_products.values()]), "produktów.")
+
+print("nowa zmiana")
